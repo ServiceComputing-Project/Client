0be13ce8a5107e73633a39b7ddcbecf9de1f1c55
zhangxy0 <43580169+zhangxy0@users.noreply.github.com>
2020-12-22 15:26:02 +0800

    优化布局

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,24 @@
 <template>
     <div id="app">
-        <nav-bar></nav-bar>
-        <div id="main">
-            <router-view></router-view>
-        </div>
         <footer-bar></footer-bar>
+        <router-view></router-view>
     </div>
 </template>
 
 <script>
-    import navBar from "./components/navBar.vue"
     import footerBar from "./components/footerBar.vue";
     export default {
         name: 'app',
         components: {
-            navBar,
             footerBar
         }
     }
 </script>
 
 <style scoped>
-    
-    #main{
-        min-height:100%;
-        padding-bottom: 260px;
+    #app{
         background-image: url('assets/background.jpg');
-        background-attachment:fixed; 
-        background-size:cover;
-        
+        background-attachment:scroll; 
+        background-size:cover;        
     }
-    
 </style>
